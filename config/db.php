deb793038cde9dcb7f07a31c39b88f32a249a4a5
tofarias <tiago.farias.poa@gmail.com>
2019-08-02 14:55:42 -0300

    Add config

---
+++ config/db.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'development' => [
+        'driver' => getenv('DB_DRIVER'),
+        'host' => getenv('DB_HOST'),
+        'database' => getenv('DB_DATABASE'),
+        'username' => getenv('DB_USERNAME'),
+        'password' => getenv('DB_PASSWORD'),
+        'charset' => 'utf8',
+        'collation' => 'utf8_unicode_ci'
+    ]
+];
