0b6b56a4c9e8d038246a5df49635b6939847b2b1
tofarias <tiago.farias.poa@gmail.com>
2019-08-02 15:09:56 -0300

    Add configurando phinx

---
+++ phinx.php
@@ -0,0 +1,44 @@
+<?php
+
+require __DIR__.'/vendor/autoload.php';
+
+if( file_exists(__DIR__.'/.env') ){
+    $dotEnv = new \Dotenv\Dotenv(__DIR__);
+    $dotEnv->overload();
+}
+
+$db = include __DIR__.'/config/db.php';
+
+list(
+    'driver'    => $adapter,
+    'host'      => $host,
+    'database'  => $name,
+    'username'  => $user,
+    'password'  => $pass,
+    'charset'   => $charset,
+    'collation' => $collation
+    ) = $db['development'];
+
+return [
+  'paths' => [
+      'migrations' => [
+          __DIR__.'/db/migrations'
+      ],
+      'seeds' => [
+          __DIR__.'/db/seeds'
+      ]
+  ],
+    'environments' => [
+        'default_migration_table' => 'migrations',
+        'default_database' => 'development',
+        'development' => [
+            'adapter'   => $adapter,
+            'host'      => $host,
+            'name'      => $name,
+            'user'      => $user,
+            'pass'      => $pass,
+            'charset'   => $charset,
+            'collation' => $collation
+        ]
+    ]
+];
