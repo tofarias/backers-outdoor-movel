e1513f183322af1259fc9c7cf26f6f7b1d68cd2c
tofarias <tiago.farias.poa@gmail.com>
2019-08-16 10:02:19 -0300

    Fix configurando envio de email

--- public/index.php
+++ public/index.php
@@ -8,8 +8,8 @@ if( file_exists(__DIR__.'/../.env') ){
     $dotEnv->overload();
 }
 
-if( file_exists(__DIR__.'/../mail.config.env') ){
-    $dotEnv = \Dotenv\Dotenv::create(__DIR__,'/../mail.config.env');
+if( file_exists(__DIR__.'/../.mail.config.env') ){
+    $dotEnv = \Dotenv\Dotenv::create(__DIR__,'/../.mail.config.env');
     $dotEnv->overload();
 }
 
--- src/Plugins/SendMailPlugin.php
+++ src/Plugins/SendMailPlugin.php
@@ -10,67 +10,41 @@ class SendMailPlugin implements PluginInterface
 {
     public function register(ServiceContainerInterface $container)
     {
-        //Create a new PHPMailer instance
         $mail = new PHPMailer(false);
-        //Tell PHPMailer to use SMTP
-        $mail->isSMTP(true);
+
+        $mail->isSMTP( env('IS_SMTP') );
         //Enable SMTP debugging
         // 0 = off (for production use)
         // 1 = client messages
         // 2 = client and server messages
-        $mail->SMTPDebug = 0;
-        //Set the hostname of the mail server
-        $mail->Host = 'smtp.gmail.com';
-        // use
-        // $mail->Host = gethostbyname('smtp.gmail.com');
-        // if your network does not support SMTP over IPv6
-        //Set the SMTP port number - 587 for authenticated TLS, a.k.a. RFC4409 SMTP submission
-        $mail->Port = 587;
-        //Set the encryption system to use - ssl (deprecated) or tls
-        $mail->SMTPSecure = 'tls';
-        //Whether to use SMTP authentication
-        $mail->SMTPAuth = true;
-        //Username to use for SMTP authentication - use full email address for gmail
-        $mail->Username = "tiago.farias.poa@gmail.com";
-        //Password to use for SMTP authentication
-        $mail->Password = "Syncmaster743n1";
-        //Set who the message is to be sent from
-        $mail->setFrom('tiago.farias.poa@gmail.com', 'First Last');
+        $mail->SMTPDebug = env('SMTP_DEBUG');
+
+        $mail->Host = env('HOST');
+
+        $mail->Port = env('PORT');
+
+        $mail->SMTPSecure = env('SMTP_SECURE');
+
+        $mail->SMTPAuth = env('SMTP_AUTH');
+
+        $mail->Username = env('USERNAME');
+
+        $mail->Password = 'PASSWORD';
+
+        $mail->setFrom(env('FROM_EMAIL'), env('FROM_NAME'));
         //Set an alternative reply-to address
         //$mail->addReplyTo('replyto@example.com', 'First Last');
         //Set who the message is to be sent to
-        $mail->addAddress('tiago.farias.poa@gmail.com', 'John Doe');
-        //Set the subject line
-        $mail->Subject = 'PHPMailer GMail SMTP test';
-        //Read an HTML message body from an external file, convert referenced images to embedded,
-        //convert HTML into a basic plain-text alternative body
-        //$mail->msgHTML(file_get_contents('contents.html'), __DIR__);
-        //Replace the plain text body with one created manually
-        //$mail->AltBody = 'This is a plain-text message body';
-        //Attach an image file
-        //$mail->addAttachment('images/phpmailer_mini.png');
-        //send the message, check for errors
+        $mail->addAddress(env('TO_EMAIL'), env('TO_NAME'));
 
-        $mail->isHTML(true);                                  // Set email format to HTML
-        
+        $mail->Subject = env('SUBJECT');
 
-        /*
-        if (!$mail->send()) {
-            echo "Mailer Error: " . $mail->ErrorInfo;
-        } else {
-            echo "Message sent!";
-            //Section 2: IMAP
-            //Uncomment these to save your message in the 'Sent Mail' folder.
-            #if (save_mail($mail)) {
-            #    echo "Message saved!";
-            #}
+        $mail->isHTML( env('IS_HTML') );
 
-        */
-            $container->addLazy(
-                'mail', function (ContainerInterface $container) use ($mail) {
-                    return $mail;
-                }
-            );
-        //}
+        $container->addLazy(
+            'mail', function (ContainerInterface $container) use ($mail) {
+                return $mail;
+            }
+        );
     }
 }
